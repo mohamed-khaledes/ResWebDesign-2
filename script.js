adba4ba4533d6d1a3305c38af84dc91755ebe396
mohamed khaled <mk6601152@gmail.com>
2022-05-14 11:55:41 +0200

    added the js file

---
+++ script.js
@@ -0,0 +1,10 @@
+let header = document.querySelector("header");
+
+window.addEventListener("scroll" , function(){
+    // let hHeader =header.getBoundingClientRect().height;
+
+    header.style.cssText="positon:fixed;"
+});
+
+
+
